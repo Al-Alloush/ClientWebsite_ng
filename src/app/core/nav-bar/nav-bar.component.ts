1d72b93a2f70e434b91c2bef861774a66504a423
Ahmad AlAlloush <ahmad.alloush@gmail.com>
2020-12-11 14:00:34 +0100

    add the userName instead of the login and register buttons after login

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
+import { IUser } from 'src/app/models/identity/user';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  currentUser$!: Observable<IUser>;
+
+  constructor( private accountService: AccountService) { }
 
   ngOnInit(): void {
+
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
 }
