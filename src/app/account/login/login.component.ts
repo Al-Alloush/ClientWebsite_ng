b0c206efa49e721fc06ac2f7c978e732ec29923a
Ahmad AlAlloush <ahmad.alloush@gmail.com>
2020-12-11 11:36:31 +0100

    get login  data from FormGroup in Html and submit this form

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  // create Reavtive angular FormGroup
+  loginForm!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm(){
+    
+    // construct our form inside our component.
+    // create FormControlls
+    this.loginForm = new FormGroup({
+      userNameOrEmail: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+  onSubmit(){
+    console.log(this.loginForm.value);
   }
 
 }
